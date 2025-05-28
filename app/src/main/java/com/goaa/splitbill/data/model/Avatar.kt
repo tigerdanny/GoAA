3894d5ac95594b5543a898e99b96eaa300e9fc13
tigerdanny <49668509+tigerdanny@users.noreply.github.com>
2025-05-28 10:56:27 +0800

    重新設計頭像為日式動漫可愛風格 - 男女動漫角色、萌系貓咪、可愛狗狗，包含個性化描述和粉色主題UI

--- app/src/main/java/com/goaa/splitbill/data/model/Avatar.kt
+++ app/src/main/java/com/goaa/splitbill/data/model/Avatar.kt
@@ -13,33 +13,33 @@ data class Avatar(
 
 object DefaultAvatars {
     val avatars = listOf(
-        // 男性頭像
-        Avatar("male_01", AvatarType.MALE, "avatar_male_01", "男性頭像 1"),
-        Avatar("male_02", AvatarType.MALE, "avatar_male_02", "男性頭像 2"),
-        Avatar("male_03", AvatarType.MALE, "avatar_male_03", "男性頭像 3"),
-        Avatar("male_04", AvatarType.MALE, "avatar_male_04", "男性頭像 4"),
-        Avatar("male_05", AvatarType.MALE, "avatar_male_05", "男性頭像 5"),
+        // 男性頭像 - 日式動漫風格
+        Avatar("male_01", AvatarType.MALE, "avatar_male_01", "陽光少年"),
+        Avatar("male_02", AvatarType.MALE, "avatar_male_02", "酷帥男主"),
+        Avatar("male_03", AvatarType.MALE, "avatar_male_03", "溫柔王子"),
+        Avatar("male_04", AvatarType.MALE, "avatar_male_04", "熱血青春"),
+        Avatar("male_05", AvatarType.MALE, "avatar_male_05", "神秘騎士"),
         
-        // 女性頭像
-        Avatar("female_01", AvatarType.FEMALE, "avatar_female_01", "女性頭像 1"),
-        Avatar("female_02", AvatarType.FEMALE, "avatar_female_02", "女性頭像 2"),
-        Avatar("female_03", AvatarType.FEMALE, "avatar_female_03", "女性頭像 3"),
-        Avatar("female_04", AvatarType.FEMALE, "avatar_female_04", "女性頭像 4"),
-        Avatar("female_05", AvatarType.FEMALE, "avatar_female_05", "女性頭像 5"),
+        // 女性頭像 - 日式動漫風格
+        Avatar("female_01", AvatarType.FEMALE, "avatar_female_01", "甜美少女"),
+        Avatar("female_02", AvatarType.FEMALE, "avatar_female_02", "元氣女孩"),
+        Avatar("female_03", AvatarType.FEMALE, "avatar_female_03", "優雅公主"),
+        Avatar("female_04", AvatarType.FEMALE, "avatar_female_04", "學園女神"),
+        Avatar("female_05", AvatarType.FEMALE, "avatar_female_05", "魔法少女"),
         
-        // 貓咪頭像
-        Avatar("cat_01", AvatarType.CAT, "avatar_cat_01", "可愛貓咪 1"),
-        Avatar("cat_02", AvatarType.CAT, "avatar_cat_02", "可愛貓咪 2"),
-        Avatar("cat_03", AvatarType.CAT, "avatar_cat_03", "可愛貓咪 3"),
-        Avatar("cat_04", AvatarType.CAT, "avatar_cat_04", "可愛貓咪 4"),
-        Avatar("cat_05", AvatarType.CAT, "avatar_cat_05", "可愛貓咪 5"),
+        // 貓咪頭像 - 日式萌系風格
+        Avatar("cat_01", AvatarType.CAT, "avatar_cat_01", "招財喵喵"),
+        Avatar("cat_02", AvatarType.CAT, "avatar_cat_02", "賣萌小貓"),
+        Avatar("cat_03", AvatarType.CAT, "avatar_cat_03", "傲嬌貓主"),
+        Avatar("cat_04", AvatarType.CAT, "avatar_cat_04", "愛心貓咪"),
+        Avatar("cat_05", AvatarType.CAT, "avatar_cat_05", "睡眠貓神"),
         
-        // 狗狗頭像
-        Avatar("dog_01", AvatarType.DOG, "avatar_dog_01", "可愛狗狗 1"),
-        Avatar("dog_02", AvatarType.DOG, "avatar_dog_02", "可愛狗狗 2"),
-        Avatar("dog_03", AvatarType.DOG, "avatar_dog_03", "可愛狗狗 3"),
-        Avatar("dog_04", AvatarType.DOG, "avatar_dog_04", "可愛狗狗 4"),
-        Avatar("dog_05", AvatarType.DOG, "avatar_dog_05", "可愛狗狗 5")
+        // 狗狗頭像 - 日式可愛風格
+        Avatar("dog_01", AvatarType.DOG, "avatar_dog_01", "忠犬小八"),
+        Avatar("dog_02", AvatarType.DOG, "avatar_dog_02", "柴犬君君"),
+        Avatar("dog_03", AvatarType.DOG, "avatar_dog_03", "秋田美男"),
+        Avatar("dog_04", AvatarType.DOG, "avatar_dog_04", "萌犬王子"),
+        Avatar("dog_05", AvatarType.DOG, "avatar_dog_05", "療癒天使")
     )
     
     fun getAvatarsByType(type: AvatarType): List<Avatar> {
